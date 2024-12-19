2c0ed34cfefc30123b829abe981851223522fb9f
jefersonprimer <jefersonpriimer@gmail.com>
2024-12-19 17:52:01 -0300

    feat(post): create record FoodRequestDTO

--- src/main/java/com/example/menu/controller/FoodController.java
+++ src/main/java/com/example/menu/controller/FoodController.java
@@ -2,11 +2,10 @@ package com.example.menu.controller;
 
 import com.example.menu.food.Food;
 import com.example.menu.food.FoodRepository;
+import com.example.menu.food.FoodRequestDTO;
 import com.example.menu.food.FoodResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,11 +15,17 @@ public class FoodController {
 
     @Autowired
     private FoodRepository repository;
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
 
     @GetMapping
     public List<FoodResponseDTO> getAll() {
 
-        List<Food> foodList = repository.findAll().stream().map(FoodResponseDTO::new);
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new);
         return foodList;
     }
 }
